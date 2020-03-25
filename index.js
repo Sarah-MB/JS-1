1df8d5c21cb386f51cb15238ac909ded81658f10
Saratu Muazu <sarahembee71@gmail.com>
2020-03-25 09:38:35 -0700

    JavaScript-1

---
+++ index.js
@@ -0,0 +1,16 @@
+// VARIABLESS
+    var name = "Sarah";
+    var courses = ["JavaScript", "Node_js", "PHP"];
+    console.log(name);
+    console.log(courses);
+
+// ODD NUMBER SECTION
+    console.log("Odd Numbers");
+    for (courses = 1; courses <= 200; courses++) {  
+    if (courses % 2 !== 0){    
+        console.log(courses);  
+    }}
+
+
+
+
